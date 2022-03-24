0206f0bb9fe4256f05877b7305f4807da9959c57
PriyalPB <102046681+PriyalPB@users.noreply.github.com>
2022-03-24 11:18:05 +0530

    Created project1.1.cpp
    It prints multiplication of two numbers.

---
+++ project1.1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+float a,b,c;
+cout<<"\nEnter the two numbers :-";
+cin>>a>>b;
+c=a*b;
+cout<<"The product of "<<a<<" and "<< b <<" = "<<c<<endl;
+}
